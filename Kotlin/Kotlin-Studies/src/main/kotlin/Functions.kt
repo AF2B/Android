5c5464a3b77a6346c3b3b21b8f267125db6d7be8
andre.borba <dev_andre.filipe@outlook.com>
2022-08-07 15:08:31 -0300

    funções

---
+++ Kotlin/Kotlin-Studies/src/main/kotlin/Functions.kt
@@ -0,0 +1,11 @@
+fun main() {
+  // Tipo de retorno
+  // Unit -> void
+  // Double, Int, String, Boolean -> tipo primitivo
+  // Array, List, Set, Map -> tipo de coleção
+  // Char, Byte, Short, Long -> tipo de número
+
+  fun imprimirSoma(a: Int, b: Int): Unit {
+    println(a + b)
+  }
+}

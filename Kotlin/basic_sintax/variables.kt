bf671d44243a965917bc4d395a0ec03e4e9c5c5a
andre.borba <dev_andre.filipe@outlook.com>
2022-08-07 03:18:44 -0300

    variáveis

---
+++ Kotlin/basic_sintax/variables.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+    var nome: String = "André"
+    var idade: Int = 24
+    var salario: Double = 1024.84
+    var casado: Boolean = false
+    var cidade: String? = null
+    var lista: List<String> = listOf("André", "Beatriz", "Nilza")
+    var lista2: MutableList<String> = mutableListOf("Filipe", "Fonsêca", "Borba")
+    var mapa: Map<String, String> = mapOf("nome" to "Borba", "sobrenome" to "Fonsêca")
+    var mapa2: MutableMap<String, String> = mutableMapOf("nome" to "André", "sobrenome" to "Borba")
+    var set: Set<String> = setOf("Beatriz", "Nilza", "Isabelle")
+    var set2: MutableSet<String> = mutableSetOf("Nilza", "Sybelle", "Almir")
+    var conjunto: Set<Int> = setOf(1, 2, 3, 4, 5)
+    var conjunto2: MutableSet<Int> = mutableSetOf(1, 2, 3, 4, 5)
+    var conjunto3: MutableSet<Int> = mutableSetOf(1, 2, 3, 4, 5)
+    var conjunto4: MutableSet<Int> = mutableSetOf(1, 2, 3, 4, 5)
+    var conjunto5: MutableSet<Int> = mutableSetOf(1, 2, 3, 4, 5)
+    var conjunto6: MutableSet<Int> = mutableSetOf(1, 2, 3, 4, 5)
+    var conjunto7: MutableSet<Int> = mutableSetOf(1, 2, 3, 4, 5)
+    var conjunto8: MutableSet<Int> = mutableSetOf(1, 2, 3, 4, 5)
+    var conjunto9: MutableSet<Int> = mutableSetOf
+    var conjunto10: MutableSet<Int> = mutableSetOf
+}

cf2b32b3688bdb27a35ebaab2051cd127ec7aa22
andre.borba <dev_andre.filipe@outlook.com>
2022-08-07 16:21:24 -0300

    for

---
+++ Kotlin/Kotlin-Studies/src/main/kotlin/For.kt
@@ -0,0 +1,14 @@
+fun main() {
+  for (i in 1...5 step 2) {
+    println(i)
+  }
+
+  val name = "andreborba"
+  for (Char in name) {
+    println("$Char")
+  }
+
+  for (i in 0..20 downTo 0 step 2) {
+    println(i)
+  }
+}

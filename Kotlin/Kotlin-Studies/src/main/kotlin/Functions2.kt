a1c19e5e2c53f3fd3ff7b9b6e4b66258cf373e86
andre.borba <dev_andre.filipe@outlook.com>
2022-08-07 15:31:05 -0300

    funções math

---
+++ Kotlin/Kotlin-Studies/src/main/kotlin/Functions2.kt
@@ -0,0 +1,16 @@
+import kotlin.math.*
+
+fun main() {
+  println(max(1, 2))
+  println(max(2, 1))
+  println(min(1, 2))
+  println(sqrt(45.78f))
+  println(PI)
+  println(E)
+  println(abs(-1))
+  println(pow(2.0, 3.0))
+  println(round(3.141592653589793))
+  println(ceil(3.14))
+  println(floor(3.14))
+  println(sin(PI / 2))
+}

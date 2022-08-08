b7a499b384fa627ec016755ab313f4d3d1691b71
andre.borba <dev_andre.filipe@outlook.com>
2022-08-07 23:21:25 -0300

    tests

--- Kotlin/UnitTests/src/test/kotlin/MainTest.kt
+++ Kotlin/UnitTests/src/test/kotlin/MainTest.kt
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.*
 
 class MainTest {
   @Test
-  @DisplayName("Test example")
+  @DisplayName("transformStringToArray")
   fun transformStringToArrayTest(){
     val input = "a,b,c,d,e"
     val expected = println(input)
@@ -10,7 +10,16 @@ class MainTest {
     Assertions.assertEquals(expected, whenCalled)
   }
 
-  
+  //If transformStringToArray receive a string with a -,
+  //It should return a message saying that the string is invalid!
+  @Test
+  @DisplayName("transformStringToArray Invalid input")
+  fun transformStringToArrayInvalidInputTest(){
+    val input = "a.b.c.d.e"
+    val expected = println("Virgula ou espaço!")
+    val whenCalled = transformStringToArray(input)
+    Assertions.assertEquals(expected, whenCalled)
+  }
 
   @Test
   @Disabled

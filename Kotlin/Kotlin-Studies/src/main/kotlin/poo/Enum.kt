2eeded48051b21ad3358c172b29f9f8a1961a98b
andre.borba <dev_andre.filipe@outlook.com>
2022-08-13 21:54:18 -0300

    Enum class Kotlin

---
+++ Kotlin/Kotlin-Studies/src/main/kotlin/poo/Enum.kt
@@ -0,0 +1,23 @@
+package poo
+
+enum class Prioridade {
+  BAIXA, MEDIA, ALTA
+}
+
+enum class Animais {
+  CACHORRO, GATO, VACA, PORCO, PEIXE, OUTRO
+}
+
+fun x(prioridade: Prioridade) {
+  when (prioridade) {
+    Prioridade.BAIXA -> println("Baixa")
+    Prioridade.MEDIA -> println("Media")
+    Prioridade.ALTA -> println("Alta")
+  }
+}
+
+fun main() {
+  x(Prioridade.ALTA)
+  x(Prioridade.MEDIA)
+  x(Prioridade.BAIXA)
+}

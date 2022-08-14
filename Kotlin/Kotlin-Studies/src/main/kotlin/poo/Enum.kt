8e0629e06fa2acce104e0f6d18f14a4d879cde99
andre.borba <dev_andre.filipe@outlook.com>
2022-08-13 22:05:26 -0300

    Enum class Kotlin

--- Kotlin/Kotlin-Studies/src/main/kotlin/poo/Enum.kt
+++ Kotlin/Kotlin-Studies/src/main/kotlin/poo/Enum.kt
@@ -8,6 +8,18 @@ enum class Animais {
   CACHORRO, GATO, VACA, PORCO, PEIXE, OUTRO
 }
 
+enum class Cor(val rgb: Int) {
+  VERMELHO(0xFF0000),
+  VERDE(0x00FF00),
+  AZUL(0x0000FF),
+  AMARELO(0xFFFF00),
+  ROSA(0xFF00FF),
+  LARANJA(0xFFA500),
+  CINZA(0x808080),
+  PRETO(0x000000),
+  BRANCO(0xFFFFFF)
+}
+
 fun x(prioridade: Prioridade) {
   when (prioridade) {
     Prioridade.BAIXA -> println("Baixa")
@@ -17,6 +29,12 @@ fun x(prioridade: Prioridade) {
 }
 
 fun main() {
+  for (cores in Cor.values()) {
+    if(cores.rgb == 0xFF0000) {
+      println("Vermelho \n")
+    }
+  }
+
   x(Prioridade.ALTA)
   x(Prioridade.MEDIA)
   x(Prioridade.BAIXA)

3da36faadd9e85b06a33a67331e45d6e843ef8a6
andre.borba <dev_andre.filipe@outlook.com>
2022-08-15 17:19:02 -0300

    explicação let kotlin

--- Kotlin/Kotlin-Studies/src/main/kotlin/Let.kt
+++ Kotlin/Kotlin-Studies/src/main/kotlin/Let.kt
@@ -5,3 +5,7 @@ fun main() {
     println(it)
   }
 }
+
+// Explicação do da função let:
+// let é uma função que recebe um lambda como parâmetro e executa o lambda apenas se o valor do
+// parâmetro não for nulo.
